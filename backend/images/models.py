821046fda364a3b57a4d22a48f80a0eacef640a0
MACBOOK PRO <macbookpro@MACBOOKs-MBP.lan>
2022-11-15 02:12:17 +0100

    migration

--- backend/images/models.py
+++ backend/images/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    name = models.CharField(max_length=50)
+    image = models.ImageField(height_field=None, width_field=None)
